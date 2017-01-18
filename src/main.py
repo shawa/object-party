a926428244a697f19d4cd6ef4299bda87187533d
shawa <andrewshaw.mail@gmail.com>
2017-01-18 23:55:15 +0000

    Add copious logging. Log levels need tweaking.

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from typing import Dict, Union, List
 
 import time
 import os
+import logging
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -24,37 +25,53 @@ ABOUT_CONFIG = {
     'extensions.netmonitor.har.autoConnect': True,
 }  # type: Dict[str, Union[bool, str]]
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+ch.setLevel(logging.DEBUG)
+logger.addHandler(ch)
 
 def get_driver(config: Dict[str, Union[bool, str]],
                extensions: List[str]) -> webdriver.firefox.webdriver.WebDriver:
     profile = webdriver.FirefoxProfile()
+    logger.info('Created new Firefox Profile')
 
     for extension in extensions:
         profile.add_extension(extension)
+        logger.info("Added extension {}".format(extension))
 
     for k, v in config.items():
         profile.set_preference(k, v)
+        logger.info('Set `{}` to `{}`'.format(k, v))
 
     driver = webdriver.Firefox(profile)
+    logger.info('Launched new Firefox driver with custom profile')
 
     return driver
 
 
 def crawl(driver, urls):
-    def toggle_network_panel(driver):
+    def toggle_network_panel(driver, delay=1):
         driver.find_element_by_tag_name('body').send_keys(Keys.META + Keys.ALT + 'q')
-        time.sleep(2)
+        time.sleep(delay)
 
     # initial setup, we need the network panel open
     for url in urls:
+        logger.info("Fetching {}".format(url))
         driver.get(url)
+        logger.info("Fetched  {}".format(url))
         toggle_network_panel(driver)
+        logger.info("Opened dev tools")
         driver.refresh()
+        logger.info("Refreshed")
         time.sleep(10)
-        toggle_network_panel(driver)
+        logger.info("Closing dev tools")
+        toggle_network_panel(driver, delay=0)
 
 
 def main():
+    logger.info('Launched crawler')
     driver = get_driver(ABOUT_CONFIG, EXTENSIONS)
 
     urls = [
