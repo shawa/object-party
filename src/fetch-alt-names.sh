abe1707bf679f0349c64026efd373995da32e101
shawa <andrewshaw.mail@gmail.com>
2017-01-23 23:29:15 +0000

    comment the second of the two mad pipelines

--- src/fetch-alt-names.sh
+++ src/fetch-alt-names.sh
@@ -7,11 +7,11 @@ readonly COUNTED_DOMAINS_DIR="counted-domains"
 readonly FIRST_PARTY="$(jq --raw-output '.domain' < "$INFILE")"
 
 true |
-    openssl s_client -connect "$FIRST_PARTY:443" 2> /dev/null |
-    openssl x509 -noout -text 2> /dev/null |
-    grep 'DNS:' |
-    sed -r 's/ ?DNS://g' |
-    sed 's/^ *//' |
-    tr -d '\n' |
-    jq --raw-input --slurp --arg 'FIRST_PARTY' "$FIRST_PARTY"\
+    openssl s_client -connect "$FIRST_PARTY:443" 2> /dev/null | # retrieve cert
+    openssl x509 -noout -text 2> /dev/null |                    # read cert to stdout
+    grep 'DNS:' |                                               # find domain entries
+    sed -r 's/ ?DNS://g' |                                      # filter to comma-separated domains
+    sed 's/^ *//' |                                             # remove leading whitespace
+    tr -d '\n' |                                                # remove traling newline
+    jq --raw-input --slurp --arg 'FIRST_PARTY' "$FIRST_PARTY"\  # write out as json
         '. |split(",") | { domain: $FIRST_PARTY, alts: .}'
