7f8fee83f9f6f789da738707ce4a7d3beec51efd
shawa <andrewshaw.mail@gmail.com>
2017-02-01 16:04:10 +0000

    Add script to count % pushable objects, % pushable data

---
+++ src/compute-pushability-stats.py
@@ -0,0 +1,56 @@
+import clize
+import json
+import os
+import csv
+import sys
+
+def fraction_pushable_objects(dataset):
+    content_bytes = {
+        'pushable': 0,
+        'third_party': 0,
+    }
+
+    objects = {
+        'pushable': 0,
+        'third_party': 0,
+    }
+
+    for obj in dataset['objects']:
+        key = 'pushable' if obj['is_first_party'] else 'third_party'
+        content_bytes[key] += obj['bytes']
+        objects[key] += obj['count']
+
+    total_objects = sum(objects.values())
+    total_bytes = sum(content_bytes.values())
+
+    pushable_objects = objects['pushable'] / total_objects
+    pushable_bytes = content_bytes['pushable'] / total_bytes
+
+    first_party = dataset['domain']
+    return first_party, pushable_objects, pushable_bytes
+
+
+def analyse_batch_data(origin_stats_dir):
+    for dataset_file in os.listdir(origin_stats_dir):
+        with open(f'{origin_stats_dir}/{dataset_file}') as f:
+            dataset = json.load(f)
+            yield fraction_pushable_objects(dataset)
+
+def main(origin_stats_dir):
+    """For a dataset of domains, batch process the domain stats files
+    from a list of [domain, occurences, response size, pushability] into
+    rows of [first party domain, fraction pushable objects, fraction pushable
+    bytes
+
+    origin_stats_dir: location of the processed stats file
+
+    """
+    writer = csv.writer(sys.stdout, delimiter=',')
+    writer.writerow(["first party domain name",
+                     "fraction pushable objects",
+                     "fraction pushable bytes"])
+    for row in analyse_batch_data(origin_stats_dir):
+        writer.writerow(row)
+
+if __name__ == '__main__':
+    clize.run(main)
